3e4b283a18768c69209ead0402ea50bd1b7954f4
Irvin <irvin.l@berkeley.edu>
2024-08-22 00:22:29 -0700

    finished player functionality

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 import pygame
 
 from constants import *
+from player import Player
 
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    time = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
-        screen.fill("black")
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        for u in updatable:
+            u.update(dt)
+
+        screen.fill("black")
+
+        for d in drawable:
+            d.draw(screen)
+
+        pygame.display.flip()
+        dt = time.tick(60) / 1000
 
 
 if __name__ == "__main__":
